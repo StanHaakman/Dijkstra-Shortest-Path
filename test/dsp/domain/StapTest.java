15741922aea6be15f6a5638e3f2e120c2ab6c5ca
Stan Haakman <stanhaakman@icloud.com>
2021-06-01 08:42:46 +0200

    Create test map en eerste template voor de stappen

---
+++ test/dsp/domain/StapTest.java
@@ -0,0 +1,20 @@
+package dsp.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StapTest {
+
+    @Test
+    public void getOther() {
+    }
+
+    @Test
+    public void getLocatieLinks() {
+    }
+
+    @Test
+    public void getLocatieRechts() {
+    }
+}
